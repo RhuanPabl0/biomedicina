d1c2f88ed8b46036bd57d822ba91ca9a6a26b816
RhuanPabl0 <rhuanpnascimento@gmail.com>
2023-11-29 16:23:59 -0400

    Liberando requisições externas

--- src/main/java/com/fasipe/biomedicina/Controllers/AgendaController.java
+++ src/main/java/com/fasipe/biomedicina/Controllers/AgendaController.java
@@ -3,10 +3,11 @@ package com.fasipe.biomedicina.Controllers;
 import com.fasipe.biomedicina.Repositorys.AgendaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin(origins = "*", originPatterns = "*")
 @RestController
 @RequestMapping("/agenda")
 public class AgendaController {
--- src/main/java/com/fasipe/biomedicina/Controllers/PacienteController.java
+++ src/main/java/com/fasipe/biomedicina/Controllers/PacienteController.java
@@ -5,10 +5,11 @@ import com.fasipe.biomedicina.Repositorys.PacienteRepository;
 import com.fasipe.biomedicina.Requests.RequestPaciente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin(origins = "*", originPatterns = "*")
 @RestController
 @RequestMapping("/paciente")
 public class PacienteController {
