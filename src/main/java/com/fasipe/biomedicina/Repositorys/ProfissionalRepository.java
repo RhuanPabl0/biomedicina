36fec37ab8425ce1d2c09dbeda7c878abf09babf
RhuanPabl0 <rhuanpnascimento@gmail.com>
2023-12-05 08:22:34 -0400

    DELETE - profissional

--- src/main/java/com/fasipe/biomedicina/Repositorys/ProfissionalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fasipe.biomedicina.Repositorys;
-
-import com.fasipe.biomedicina.Entitys.Profissional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfissionalRepository extends JpaRepository<Profissional,Long> {
-}
